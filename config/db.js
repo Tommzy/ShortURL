2860f8eb6532d4f7f81846985b0e34a054fb7837
Tommzy <hzheng@wpi.edu>
2016-07-10 05:00:00 -0400

    update the db config

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : 'mongodb://heroku_0bhc37wk:7f566uk7drd1e9ebl7arre9ret@ds017205.mlab.com:17205/heroku_0bhc37wk'
+    url : 'process.env.MONGOLAB_URI ||' +
+    ' mongodb://heroku_0bhc37wk:7f566uk7drd1e9ebl7arre9ret@ds017205.mlab.com:17205/heroku_0bhc37wk'
 }
