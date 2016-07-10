98fa8e921043d6a1dcbe9ebd4feed777149974ab
Tommzy <hzheng@wpi.edu>
2016-07-10 05:27:45 -0400

    at least local works

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : ' mongodb://heroku_0bhc37wk:7f566uk7drd1e9ebl7arre9ret@ds017205.mlab.com:17205/heroku_0bhc37wk'
+    url : 'mongodb://heroku_0bhc37wk:7f566uk7drd1e9ebl7arre9ret@ds017205.mlab.com:17205/heroku_0bhc37wk'
 }
