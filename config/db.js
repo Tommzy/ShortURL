d9f598f057af241c77cfd9adfba79caceb2530fc
Tommzy <hzheng@wpi.edu>
2016-07-10 04:52:19 -0400

    change db config

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : 'mongodb://hzheng@wpi.edu:990523fy@ds017205.mlab.com:17205/heroku_0bhc37wk'
+    url : 'mongodb://heroku_0bhc37wk:7f566uk7drd1e9ebl7arre9ret@ds017205.mlab.com:17205/heroku_0bhc37wk'
 }
